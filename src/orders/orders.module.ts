f49614d0af746e3360568fedcf82857ade0ef402
Daymio1 <alexandru-razvanmuresan@outlook.com>
2023-04-14 14:19:01 +0300

    fix

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,16 @@
 import { Module } from "@nestjs/common";
 import { OrdersService } from "./orders.service";
 import { OrdersController } from "./orders.controller";
+import { Medication, MedicationsModel } from "src/medications/data_layer/medications.model";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Order, OrdersModel } from "./data_layer/orders.model";
+import { OrdersRepository } from "./data_layer/orders.repository";
+import { MedicationsService } from "src/medications/medications.service";
+import { MedicationsRepository } from "src/medications/data_layer/medications.repository";
 
 @Module({
-  providers: [OrdersService],
+  imports: [MongooseModule.forFeature([{ name: Order.name, schema: OrdersModel }]), MongooseModule.forFeature([{ name: Medication.name, schema: MedicationsModel }])],
+  providers: [OrdersService, OrdersRepository, MedicationsService, MedicationsRepository, { provide: "OrdersServiceInterface", useClass: OrdersService }],
   controllers: [OrdersController],
 })
 export class OrdersModule {}
