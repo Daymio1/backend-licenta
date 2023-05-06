9a91bf2643e6265dcfd19bb44be776ee6a5b2111
Daymio1 <alexandru-razvanmuresan@outlook.com>
2023-05-06 22:47:42 +0300

    fix file size

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import * as bodyParser from "body-parser";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
-import { json } from "express";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.use(json({ limit: "50mb" }));
+  app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+  app.use(bodyParser.json({ limit: "50mb" }));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const config = new DocumentBuilder().setTitle("Licenta").setDescription("Endpoints").setVersion("v1").build();
