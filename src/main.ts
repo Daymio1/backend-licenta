2bc40878a4462ae7d6b147eca785bfe98ab594cd
Daymio1 <alexandru-razvanmuresan@outlook.com>
2023-05-06 22:37:51 +0300

    file larger

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { json } from "express";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(json({ limit: "50mb" }));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const config = new DocumentBuilder().setTitle("Licenta").setDescription("Endpoints").setVersion("v1").build();
