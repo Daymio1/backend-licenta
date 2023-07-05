6d66a3da5463ecee0c05882602e73e0812ab04bd
Daymio1 <alexandru-razvanmuresan@outlook.com>
2023-07-06 01:09:39 +0300

    l

--- src/medications/data_layer/medications.repository.ts
+++ src/medications/data_layer/medications.repository.ts
@@ -25,7 +25,6 @@ export class MedicationsRepository {
     const sort = medicationsFilterQuery.sort && medicationsFilterQuery.sort.toString();
     const sortOption = { [field]: sort };
 
-    console.log(searchOption);
     return this.medicationsModel.find(searchOption).sort(sortOption);
   }
 
